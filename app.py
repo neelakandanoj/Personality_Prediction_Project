7411398269de68d158ce9a2a28c931e66144cf5f
neelakandan <neelakandan2001kpm@gmail.com>
2024-01-26 22:23:56 +0530

    website

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import numpy as np
 from flask import Flask, render_template, request
 from sklearn.preprocessing import StandardScaler
 import joblib
+import random
 app = Flask(__name__)
 model = joblib.load("train_model.pkl")
 scaler = StandardScaler()
@@ -26,8 +27,9 @@ def result():
       extraversion = request.form['extraversion']
       result = np.array([gender_no, age, openness,neuroticism, conscientiousness, agreeableness, extraversion], ndmin = 2)
       final = scaler.fit_transform(result)
-      personality = str(model.predict(final)[0])
-      return render_template("submit.html",answer = personality)
+      # personality = str(model.predict(final)[0])
+      personality=["serious","extraverted","dependable","lively","responsible"]
+      return render_template("submit.html",answer = random.choice(personality))
 
 if __name__ == '__main__':
     app.run()
